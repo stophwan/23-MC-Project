519f4a9a459dd01c199eca1604e712f724cdefc4
jys <tlswl313@gamil.com>
2023-06-02 15:13:15 +0900

    layout_fragment(3) 수정

--- app/src/main/java/com/example/mc_project/MainActivity.kt
+++ app/src/main/java/com/example/mc_project/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.mc_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mc_project.databinding.ActivityMainBinding
-import com.example.mc_project.databinding.FriendpageBinding
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
+import com.example.mc_project.databinding.ActivityTabBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityTabBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityTabBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val tabLayout = binding.tabLayout
+        val viewPage = binding.viewPager
 
-        binding.friendbtn.setOnClickListener { supportFragmentManager.beginTransaction()
-
-        }
+        viewPage.adapter = AdapterFragment(supportFragmentManager, lifecycle)
+        TabLayoutMediator(tabLayout, viewPage) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.friend)
+                }
+                1 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.map)
+                }
+                2 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.mypage)
+                }
+            }
+        }.attach()
     }
 }
