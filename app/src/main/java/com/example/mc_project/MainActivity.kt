13838a0a722620ab4b5268235ceaf61d14d00908
stophwan <jjw981210@gmail.com>
2023-06-07 07:02:21 +0900

    feat: 카카오 키워드 검색 api연결

--- app/src/main/java/com/example/mc_project/MainActivity.kt
+++ app/src/main/java/com/example/mc_project/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.mc_project
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import com.example.mc_project.databinding.ActivityTabBinding
+import com.example.mc_project.kakao.KakaoSearch
 import com.google.android.material.tabs.TabLayoutMediator
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.Signature
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityTabBinding
+    val kakaoSearch = KakaoSearch();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTabBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val tabLayout = binding.tabLayout
         val viewPage = binding.viewPager
+
+        kakaoSearch.searchPlaceByKeyword("맥도날드", "127.06283102249932", "37.514322572335935", 10000)
 
         viewPage.adapter = AdapterFragment(supportFragmentManager, lifecycle)
         TabLayoutMediator(tabLayout, viewPage) { tab, position ->
