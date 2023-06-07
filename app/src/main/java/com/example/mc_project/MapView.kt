741889d481c4d6d63cc6fc9e607346b68854ca20
jys <tlswl313@gamil.com>
2023-06-08 02:06:42 +0900

    카카오 맵 api 공식문서 참고하여 약간의 기본 구현 해봤습니다. 맵을 띄운 후 수정 필요 / (net.daum.mf.map.api.MapView)

---
+++ app/src/main/java/com/example/mc_project/MapView.kt
@@ -0,0 +1,29 @@
+package com.example.mc_project
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MapView(activity: AppCompatActivity)  {
+    // kakao map api 공식 문서 참고 하면서
+    // 'kakaoMapView.kt' 파일에 필요한 부분 넣다 보니 오류 발생하지 않게 하기 위해 임시적으로 넣었습니다
+    // 맵 연결 후 수정 및 삭제 필요합니다
+
+    fun addPOIItem(customMarker: Any) {
+
+    }
+    fun setMapCenterPointAndZoomLevel(mapPointWithGeoCoord: Any, i: Int, b: Boolean) {
+
+    }
+    fun setZoomLevel(i: Int, b: Boolean) {
+
+    }
+    fun setMapCenterPoint(mapPointWithGeoCoord: Any, b: Boolean) {
+
+    }
+    fun zoomIn(b: Boolean) {
+
+    }
+    fun zoomOut(b: Boolean) {
+
+    }
+
+}
